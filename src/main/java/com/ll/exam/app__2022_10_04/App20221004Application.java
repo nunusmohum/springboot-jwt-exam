1e803a4849576a42e323181bedef5aaf6381d485
nunusmohum <halojw301@gmail.com>
2022-10-04 14:24:05 +0900

    fisrt commit

---
+++ src/main/java/com/ll/exam/app__2022_10_04/App20221004Application.java
@@ -0,0 +1,14 @@
+package com.ll.exam.app__2022_10_04;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App20221004Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App20221004Application.class, args);
+	}
+
+}
